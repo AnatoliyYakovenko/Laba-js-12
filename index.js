a3285c2243a650852b66f3650d32e90ae8338f2e
Anatoliy Yakovenko <yakovenko.anatoliy@gmail.com>
2023-04-18 17:27:32 +0300

    coplete task with proto

---
+++ index.js
@@ -0,0 +1,20 @@
+Array.prototype.pow = function (n) {
+  return this.map(function (el) {
+    return Math.pow(el, n);
+  });
+};
+
+Function.prototype.defer = function (ms) {
+  let func = this;
+  return function () {
+    setTimeout(func, ms);
+  };
+};
+
+let arr = [1, 2, 3];
+console.log(arr.pow(2));
+
+function a() {
+  alert("test");
+}
+a.defer(1000)();
